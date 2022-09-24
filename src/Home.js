5f7214c42e089930ff5b5431a1a0724918cf6a09
sanemturgan <75381465+sanemturgan@users.noreply.github.com>
2022-09-24 13:17:58 +0300

    Feat:adding loading page and catch error.

--- src/Home.js
+++ src/Home.js
@@ -3,32 +3,46 @@ import BlogList from './components/BlogList';
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlogs(newBlogs);
   };
 
-  //   useEffect(() => {
-  //     fetch('http://localhost:3000/blogs')
-  //       .then((response) => {
-  //         return response.json();
-  //       })
-  //       .then((data) => {
-  //         setBlogs(data);
-  //         console.log(data);
-  //       });
-  //   }, []);
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => response.json())
-      .then((data) => {
-        setBlogs(data);
-        console.log(data);
-      });
+    setTimeout(() => {
+      fetch('https://jsonplaceholder.typicode.com/posts')
+        .then((res) => {
+          if (!res.ok) {
+            throw Error('could not fetch the data for that resource');
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
+        });
+    }, 500);
   }, []);
+  // useEffect(() => {
+  //   fetch('https://jsonplaceholder.typicode.com/posts')
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       setBlogs(data);
+  //       console.log(data);
+  //     });
+  // }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
+      {isPending && <div>Loading..</div>}
       {blogs && <BlogList blogs={blogs} handleDelete={handleDelete} />}{' '}
     </div>
   );
